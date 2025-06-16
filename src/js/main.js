adaa109b7302c7cccc46d7a10e91f07e4101f8f1
Sebastian Arnache Cantillo <sarnachecantillo@gmail.com>
2025-06-16 15:52:56 -0500

    feat: the algorimth was created.

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+let weight
+let height
+let BMI
+
+weight=prompt(message = "please, write your weight")
+height=prompt(message = "please, write your height")
+
+BMI = weight / (height * height)
+
+alert("Your BMI is " + BMI)
+
+
